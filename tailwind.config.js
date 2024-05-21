ed1aab7c0322c21b1321901021be67808282230a
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-05-22 01:06:42 +0500

    Home, headphones and speakers done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/assets/home/desktop/image-speaker-zx7.jpg')",
+      }
+    },
   },
   plugins: [require('daisyui'),],
 }
